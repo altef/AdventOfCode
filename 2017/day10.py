7cd15190d5cf2792f08c36e5b89903a660999c5a
altef <bradgill+github@gmail.com>
2017-12-14 13:39:00 -0800

    day 14 adjustments

--- 2017/day10.py
+++ 2017/day10.py
@@ -1,9 +1,6 @@
-with open('day10-input.txt', 'r') as f:
-    input = f.read()
 
 
-
-def hash(input, somenumbers, current_pos = 0, skip_size = 0):
+def hash(inputs, somenumbers, current_pos = 0, skip_size = 0):
 	l = somenumbers[:]
 	#print(l)
 	for input in inputs:
@@ -31,27 +28,38 @@ def hash(input, somenumbers, current_pos = 0, skip_size = 0):
 	return(l, current_pos, skip_size)
 
 
-# Part 1
-#inputs = [x for x in input.split(',')]
-#l, cp, ss = hash(inputs, [x for x in range(0, 256)])
-#print("Step 1: {}".format(l[0]*l[1]))
 
 
 # loop through 64 times
 #input = '1,2,4'
-inputs = [x for x in input.split(',')]
-inputs = [ord(x) for x in input] + [17, 31, 73, 47, 23]
-ss = 0
-cp = 0
-l = [x for x in range(0, 256)]
-for i in range(0, 64):
+
+def knothash(input):
+	inputs = [x for x in input.split(',')]
+	inputs = [ord(x) for x in input] + [17, 31, 73, 47, 23]
+	ss = 0
+	cp = 0
+	l = [x for x in range(0, 256)]
+	for i in range(0, 64):
 	#print("hash: {}, {}".format(cp, ss))
-	l, cp, ss = hash(inputs, l, cp, ss)
+		l, cp, ss = hash(inputs, l, cp, ss)
+	
+	# xor sequence
+	_hash = []
+	for i in range(0, len(l), 16):
+		#print("Hashing {} to {} of {}".format(i, i+16, len(l)))
+		_hash.append(reduce(lambda a,b: a^b, l[i:i+16]))
+	return "".join(["{:02x}".format(x) for x in _hash])
 
-# xor sequence
-hash = []
-for i in range(0, len(l), 16):
-	#print("Hashing {} to {} of {}".format(i, i+16, len(l)))
-	hash.append(reduce(lambda a,b: a^b, l[i:i+16]))
 
-print("Step 2: {}".format("".join(["{:02x}".format(x) for x in hash])))
+if __name__ == "__main__":
+	with open('day10-input.txt', 'r') as f:
+		input = f.read()
+
+
+	# Part 1
+	inputs = [int(x) for x in input.split(',')]
+	l, cp, ss = hash(inputs, [x for x in range(0, 256)])
+	print("Step 1: {}".format(l[0]*l[1]))
+
+
+	print("Step 2: {}".format(knothash(input)))
