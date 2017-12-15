bdc54008d42bb4aa5563fbb65e753ca229bd7eb6
altef <bradgill+github@gmail.com>
2017-12-14 22:38:04 -0800

    in case we run them both

--- 2017/day15.py
+++ 2017/day15.py
@@ -1,34 +1,30 @@
-generators = [
-	{
-		'name': 'A',
-		'last': 679,
-		'coeff': 16807,
-		'mod': 4,
-	},
-	{
-		'name': 'B',
-		'last': 771,
-		'coeff': 48271,
-		'mod': 8
-	}
-]
-
-
-
 def next(generator, respect_mod = False):
 	divisor = 2147483647
 	while True:
 		next = (generator['last'] * generator['coeff']) % divisor
 		generator['last'] = next
-		if not respect_mod or (next % generator['mod'] == 0):
+		if not respect_mod or next % generator['mod'] == 0:
 			break
 	return next
 
 
-
 def check(number, picky = False):
-	matches = 0
+	generators = [
+		{
+			'name': 'A',
+			'last': 679,
+			'coeff': 16807,
+			'mod': 4,
+		},
+		{
+			'name': 'B',
+			'last': 771,
+			'coeff': 48271,
+			'mod': 8
+		}
+	]
 	
+	matches = 0
 	for i in range(0, number):
 		o = []
 		for generator in generators:
@@ -37,6 +33,7 @@ def check(number, picky = False):
 		if all(x == o[0] for x in o):
 			matches += 1
 	return matches
+
 
 
 if __name__ == "__main__":
